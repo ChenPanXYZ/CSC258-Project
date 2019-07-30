e2c66020a5f576df142e4a34eadd3b8803ab0c36
chenpan <pan@panchen.xyz>
2019-07-29 23:07:32 -0400

    added worked top module

--- top.v
+++ top.v
@@ -587,6 +587,98 @@ endmodule
 
 
 
+// module milestone1(
+  Clock Input (50 MHz)
+  // input  CLOCK_50,
+   Push Buttons
+  // input  [3:0]  KEY,
+   DPDT Switches 
+   7-SEG Displays
+   LEDs
+  // output  [8:0]  LEDG,  //  LED Green[8:0]
+  // output  reg [17:0]  LEDR,  //  LED Red[17:0]
+   PS2 data and clock lines		
+  // input	PS2_DAT,
+  // input	PS2_CLK,
+   GPIO Connections
+  // inout  [35:0]  GPIO_0, GPIO_1
+// );
+
+ set all inout ports to tri-state
+// assign  GPIO_0    =  36'hzzzzzzzzz;
+// assign  GPIO_1    =  36'hzzzzzzzzz;
+
+// wire RST;
+// assign RST = KEY[0];
+
+
+
+turn off green LEDs
+// assign LEDG = 0;
+
+// wire reset = 1'b0;
+// wire [7:0] scan_code;
+
+// reg [7:0] history[1:4];
+// wire read, scan_ready;
+
+// oneshot pulser(
+   // .pulse_out(read),
+   // .trigger_in(scan_ready),
+   // .clk(CLOCK_50)
+// );
+
+// keyboard kbd(
+  // .keyboard_clk(PS2_CLK),
+  // .keyboard_data(PS2_DAT),
+  // .clock50(CLOCK_50),
+  // .reset(reset),
+  // .read(read),
+  // .scan_ready(scan_ready),
+  // .scan_code(scan_code)
+// );
+
+// wire [5:0] HEX0;
+// wire [5:0] HEX1;
+
+// hex_7seg dsp0(history[1][3:0],HEX0);
+// hex_7seg dsp1(history[1][7:4],HEX1);
+
+
+// always @(*)
+// begin
+	// if(HEX0 == 6'b001001 && HEX1 == 6'b1111000) begin
+		// LEDR[0] = 1;
+	// end
+	// else begin
+		// LEDR[0] = 0;
+	// end
+// end
+
+
+// always @(posedge scan_ready)
+// begin
+	// history[4] <= history[3];
+	// history[3] <= history[2];
+	// history[2] <= history[1];
+	// history[1] <= scan_code;
+// end
+
+	
+
+blank remaining digits
+// /*
+// wire [6:0] blank = 7'h7f;
+// assign HEX2 = blank;
+// assign HEX3 = blank;
+// assign HEX4 = blank;
+// assign HEX5 = blank;
+// assign HEX6 = blank;
+// assign HEX7 = blank;
+// */
+
+// endmodule
+
 
 
 
